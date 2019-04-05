3f7860f7537b18baaf95e81c070694a29d6c3fd0
Nick Knight <nick@localhost.localdomain>
2019-04-05 16:42:36 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+# flip package
+
+A package which promotes full documentation in comments. Open flip-view.js in atom and ctrl-alt-o.
+
+![A screenshot of your package](flip.png)
---
+++ spec/flip-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FlipView from '../lib/flip-view';
+
+describe('FlipView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
